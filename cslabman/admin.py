9d4638aa78d2cd90d4d83a1dce416feac7bfb498
Mario <rrn2h@mtmail.mtsu.edu>
2021-11-15 03:27:39 +0000

    generate script 10% complete

--- cslabman/admin.py
+++ cslabman/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from django.forms.models import model_to_dict
 # Register your models here.
 
 #Database models
@@ -8,6 +8,8 @@ from .models import Students
 from .models import Scripts
 from .models import AccountRequests
 from .models import RequestType
+
+
 
 #register models so they show up on Admin page and can be manipulated
 # admin.site.register(Systems)
@@ -20,13 +22,59 @@ admin.site.register(Students)
 #Below creates an action to "Generate Script" with the selection.
 # queryset is what contains the selection.
 # for acc_req in queryset: returns a row that's easy to work with.
-@admin.action(description='Generate Script')
-def generate_script(modeladmin,request,queryset):
-  scripts = Scripts.objects.all()
 
+
+def gen_search(items,key,value,key2=None,value2=None):
+
+  if key2 is not None:
+    for item in items:
+      if item[key]==value:
+        return item
+  else:
+    for item in items:
+      print(item)
+      if item[key]==value and item[key2]==value2:
+        return item
+  return None
+
+@admin.action(description='Generate Script')
+
+def generate_script(modeladmin,request,queryset):
+  systems = Systems.objects.all()
+  systems = systems.values()
+  scripts=Scripts.objects.values()
+  scripts=list(scripts)
+  print(scripts)
+  #return
+  #print (systems.objects.get(system_name='system64'))
+  script='This is the scripts you need to copy \n \n'
+  password_input="put the password here"
+  # make sure this file save in scrirpts or media
+  f=open("script.txt","w+")
+  #first=True
   for acc_req in queryset:
-    # script = scripts.
-    print(acc_req.reason)
+
+    sys=str(acc_req.system_name)
+    #rt=request type
+    rt=str(acc_req.request_type)
+    script=gen_search(scripts,'system_name_id',sys,'request_type',rt)
+
+    group_name=gen_search(systems,'system_name',sys)
+    #print(group_name['group_name'])
+    group_name=group_name['group_name']
+    script=script['script_string']
+    script_new=script.format(user=acc_req.pipeline_id,group=group_name)+'\n'
+    #script_new=('add-LocalGroupMember -Group "{}"'.format(group_name))+\
+    #(' -Member "{}"'.format(acc_req.pipeline_id))+"\n"
+    script=script+script_new
+
+  f.write(script_string)
+  queryset.update(request_complete=1)
+  queryset.update(request_status=4)
+
+  print (script_string)
+  f.close()
+
 
 
 class RequestTypeListFilter(admin.SimpleListFilter):
